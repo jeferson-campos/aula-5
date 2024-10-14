583d052fd0aabc9b2ec85b51d0174fbac56c710e
jeferson-campos <jefersoncampos5296@gmail.com>
2024-10-14 12:56:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// let cores = ["Vermelho", "Verde", "Azul", "Amarelo"];
+// console.log("O elemento na posição 3 é: " + cores[3]);
+// let animais = ["Cachorro", "Gato", "Papagaio"];
+// animais.push("Tartaruga");
+
+// animais.shift();
+// animais.unshift("Coelho");
+// animais[2] = "Hamster";
+// console.log("O comprimento atual do array é: " + animais.length);
+// console.log("O elemento na posição 1 é: " + animais[1]);
+// console.log("O array final é: ", animais);
